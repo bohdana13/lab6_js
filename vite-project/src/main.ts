410a5363c9e321c14550d420acb3e928017da58a
spring_day <bogdanaredka2005@gmail.com>
2024-10-31 15:49:43 +0200

    initial

---
+++ vite-project/src/main.ts
@@ -0,0 +1,21 @@
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import './style.css'
+import App from './App.vue'
+import Home from './views/Home.vue'
+import About from './views/About.vue'
+
+const routes = [
+    {path: "/", component : Home },
+
+    {path: "/about", component : About },
+]
+
+const router = createRouter ({
+    history: createWebHistory(),
+    routes,
+});
+
+
+
+createApp(App).use(router).mount('#app')
