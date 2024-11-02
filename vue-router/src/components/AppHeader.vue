8b5e44f6dc1fd6a4f970d434bfa353471c7ce14d
spring_day <bogdanaredka2005@gmail.com>
2024-11-02 16:37:38 +0200

    add header component

---
+++ vue-router/src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <header>
+      <h1>Lottery App</h1>
+      <AppNavigation />
+    </header>
+  </template>
+  
+  <script lang="ts">
+  import AppNavigation from './AppNavigation.vue';
+  
+  export default {
+    name: 'AppHeader',
+    components: { AppNavigation },
+  };
+  </script>
+  
